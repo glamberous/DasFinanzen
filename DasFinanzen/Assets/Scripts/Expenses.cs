8075492e2556c39bc22ef5ab04c9160798878627
glamberouos <grantwlamb@gmail.com>
2019-07-21 12:25:06 -0700

    Setting up the project...

---
+++ DasFinanzen/Assets/Scripts/Expenses.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+public struct AppData {
+    public Dictionary<int, Dictionary<int, List<Expense>>> 
+}*/
+
+public class Expense {
+    public int Day { get; set; }
+    public decimal Amount { get; set; }
+    public string Name { get; set; }
+}
+
+public class MonthData {
+    public Dictionary<int, List<Expense>> months;
+}
+
+public class YearData {
+    public Dictionary<int, MonthData> years;
+}
+
+public class Test {
+    Expense new_expense;
+    public Test() => new_expense = new Expense();
+}
