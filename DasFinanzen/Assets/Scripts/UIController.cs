da36c2c572442ec5cefb2ff86026f3fe17b11d02
glamberouos <grantwlamb@gmail.com>
2019-07-23 08:07:50 -0700

    Started on the UI Controller
    - Need to figure out some sort of data Save/Load system

--- DasFinanzen/Assets/Scripts/UIController.cs
+++ DasFinanzen/Assets/Scripts/UIController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
+    public const float CatagoryOffsetY = 10.0f; //TODO figure out what the const amount should be for this
+    public const float CatagoryTileExtend = 20.0f;
+
+    [SerializeField] private GameObject DayToDayTile;
+    [SerializeField] private GameObject ReoccurringTile;
+
     // Start is called before the first frame update
     void Start()
     {
